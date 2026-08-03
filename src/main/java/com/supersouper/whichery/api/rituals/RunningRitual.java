56d87361b4c3783adc7e93e651f49fbcac01ea1d
SuperSoupr <26164930+SuperSoupr@users.noreply.github.com>
2026-08-03 14:31:20 +0300

    Multiple animations and effects per ritual

--- src/main/java/com/supersouper/whichery/api/rituals/RunningRitual.java
+++ src/main/java/com/supersouper/whichery/api/rituals/RunningRitual.java
@@ -5,18 +5,22 @@ import java.util.UUID;
 
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
 import net.minecraft.tileentity.TileEntity;
 
 import com.supersouper.whichery.utils.WhicheryUtils;
 
 public class RunningRitual {
 
+    /** NBT tag id for a compound, as used by {@link NBTTagCompound#getTagList}. */
+    private static final int TAG_COMPOUND = 10;
+
     public final TileEntity leader;
     private Ritual ritual;
     private EntityPlayer starter;
     private UUID starterUUID;
-    private RitualEffect effect;
-    private RitualAnimation animation;
+    private RitualEffect[] effects = new RitualEffect[0];
+    private RitualAnimation[] animations = new RitualAnimation[0];
     private Long startedAt = null;
     private int timePassedInPreviousSave = 0;
     private byte rotation;
@@ -29,27 +33,34 @@ public class RunningRitual {
         this.starterUUID = starter.getUniqueID();
         this.rotation = rotation;
         this.seenStages = new boolean[ritual.stages.length];
-        constructAnimationAndEffect();
+        constructEffectsAndAnimations();
     }
 
     public RunningRitual(TileEntity leader) {
         this.leader = leader;
     }
 
-    private void constructAnimationAndEffect() {
-        try {
-            animation = ritual.animationClass.getConstructor(TileEntity.class, RunningRitual.class)
-                .newInstance(leader, this);
-        } catch (InvocationTargetException | InstantiationException | IllegalAccessException
-            | NoSuchMethodException e) {
-            throw new RuntimeException(e);
+    private void constructEffectsAndAnimations() {
+        effects = new RitualEffect[ritual.effectClasses.length];
+        for (int i = 0; i < effects.length; i++) {
+            effects[i] = instantiate(ritual.effectClasses[i]);
         }
+
+        animations = new RitualAnimation[ritual.animationClasses.length];
+        for (int i = 0; i < animations.length; i++) {
+            animations[i] = instantiate(ritual.animationClasses[i]);
+        }
+    }
+
+    private <T> T instantiate(Class<? extends T> cls) {
         try {
-            effect = ritual.effectClass.getConstructor(TileEntity.class, RunningRitual.class)
+            return cls.getConstructor(TileEntity.class, RunningRitual.class)
                 .newInstance(leader, this);
         } catch (InvocationTargetException | InstantiationException | IllegalAccessException
             | NoSuchMethodException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException(
+                cls.getName() + " must declare a public (TileEntity, RunningRitual) constructor",
+                e);
         }
     }
 
@@ -79,14 +90,22 @@ public class RunningRitual {
 
         for (int i = 0; i <= Math.min(stage, seenStages.length - 1); i++) {
             if (!seenStages[i]) {
-                effect.transitionToStage(i);
-                animation.transitionToStage(i);
+                for (RitualEffect effect : effects) {
+                    effect.transitionToStage(i);
+                }
+                for (RitualAnimation animation : animations) {
+                    animation.transitionToStage(i);
+                }
                 seenStages[i] = true;
             }
         }
 
-        effect.onTick();
-        animation.onTick();
+        for (RitualEffect effect : effects) {
+            effect.onTick();
+        }
+        for (RitualAnimation animation : animations) {
+            animation.onTick();
+        }
 
         if (stage >= ritual.stages.length) {
             ((IRitualLeader) leader).endRitual();
@@ -95,8 +114,13 @@ public class RunningRitual {
 
     public void end() {
         int stage = Math.min(getStage(), ritual.stages.length - 1);
-        effect.end(stage);
-        animation.end(stage);
+
+        for (RitualEffect effect : effects) {
+            effect.end(stage);
+        }
+        for (RitualAnimation animation : animations) {
+            animation.end(stage);
+        }
     }
 
     public int getTimePassed() {
@@ -119,12 +143,31 @@ public class RunningRitual {
         return ritual;
     }
 
+    public RitualEffect[] getEffects() {
+        return effects;
+    }
+
+    public RitualAnimation[] getAnimations() {
+        return animations;
+    }
+
     public NBTTagCompound writeToNBT(NBTTagCompound tag) {
         tag.setString("ritual", ritual.getName());
         tag.setString("starterUUID", starterUUID.toString());
         tag.setInteger("timePassedInPreviousSave", getTimePassed());
-        tag.setTag("animation", animation.writeToNBT(new NBTTagCompound()));
-        tag.setTag("effect", effect.writeToNBT(new NBTTagCompound()));
+
+        NBTTagList effectTags = new NBTTagList();
+        for (RitualEffect effect : effects) {
+            effectTags.appendTag(effect.writeToNBT(new NBTTagCompound()));
+        }
+        tag.setTag("effects", effectTags);
+
+        NBTTagList animationTags = new NBTTagList();
+        for (RitualAnimation animation : animations) {
+            animationTags.appendTag(animation.writeToNBT(new NBTTagCompound()));
+        }
+        tag.setTag("animations", animationTags);
+
         tag.setByte("rotation", rotation);
         tag.setByteArray("seenStages", WhicheryUtils.booleanArrayToByteArray(seenStages));
         return tag;
@@ -135,10 +178,20 @@ public class RunningRitual {
         timePassedInPreviousSave = tag.getInteger("timePassedInPreviousSave");
         starterUUID = UUID.fromString(tag.getString("starterUUID"));
 
-        constructAnimationAndEffect();
+        constructEffectsAndAnimations();
 
-        animation.readFromNBT(tag.getCompoundTag("animation"));
-        effect.readFromNBT(tag.getCompoundTag("effect"));
+        // A ritual's effect/animation list can change between saves, so only restore the overlap: any
+        // saved state past the end is dropped, and any new component simply starts fresh.
+        NBTTagList effectTags = tag.getTagList("effects", TAG_COMPOUND);
+        for (int i = 0; i < Math.min(effectTags.tagCount(), effects.length); i++) {
+            effects[i].readFromNBT(effectTags.getCompoundTagAt(i));
+        }
+
+        NBTTagList animationTags = tag.getTagList("animations", TAG_COMPOUND);
+        for (int i = 0; i < Math.min(animationTags.tagCount(), animations.length); i++) {
+            animations[i].readFromNBT(animationTags.getCompoundTagAt(i));
+        }
+
         rotation = tag.getByte("rotation");
         seenStages = WhicheryUtils.byteArrayToBooleanArray(tag.getByteArray("seenStages"));
     }
